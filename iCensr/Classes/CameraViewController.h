6e57a486d16c183f530377b7289cba5e36e1c4fb
Elizabeth Fuller <admin@efuller.net>
2009-06-04 03:34:03 -0400

    now with working alert views at beginning if twitter values have not yet been set

--- iCensr/Classes/CameraViewController.h
+++ iCensr/Classes/CameraViewController.h
@@ -7,14 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import "AboutViewController.h"
 #import "EditorViewController.h"
 #import "ORSTwitPicDispatcher.h"
+#import "MGTwitterEngine.h"
 
 @interface CameraViewController : UIViewController {
 	AboutViewController *aboutViewController;
 	EditorViewController *editorViewController;
-	IBOutlet UINavigationController *navigationController;
 	
 	//settings for buttons and views
 	UIImagePickerController *imagePicker; // for use with the album
@@ -23,6 +24,13 @@
 	IBOutlet UIBarItem *camera; // button sending user to the camer
 	IBOutlet UIBarItem *upload; // button allowing user to upload the image
     IBOutlet UIImageView *image; // image of the item
+	
+	// fields for login alert box
+	IBOutlet UIAlertView	*signinAlertView;
+	IBOutlet UITextField	*twtName;
+	IBOutlet UITextField	*twtPW;
+	MGTwitterEngine			*twitterEngine; //class to connect and post to twitter
+
 }
 
 @property(nonatomic,retain) AboutViewController *aboutViewController;
@@ -31,11 +39,26 @@
 @property(nonatomic,retain) UIImagePickerController *imagePicker;
 @property(nonatomic,retain) UIImagePickerController *imageTaker;
 
-@property(nonatomic,retain) IBOutlet UINavigationController *navigationController;
+@property(nonatomic,retain) IBOutlet UIAlertView *signinAlertView;
+@property(nonatomic,retain) IBOutlet UITextField *twtName;
+@property(nonatomic,retain) IBOutlet UITextField *twtPW;
 
-- (IBAction)grabImage;
-- (IBAction)takeImage;
+- (IBAction) grabImage;
+- (IBAction) takeImage;
 - (IBAction) aboutICensr:(id) sender;
+
+// login related functions
+- (BOOL) isSignedIn;
+- (void) askForLoginInfo;
+- (void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex;
+- (void) checkContent;
+- (void) checkConnection;
+- (void) saveValues;
+
+- (UITextField*)addTextFieldWithValue:(NSString*)value label:(NSString*)label;
+- (UITextField*)textFieldAtIndex:(NSUInteger)index;
+- (NSUInteger)textFieldCount;
+- (UITextField*)textField;
 
 
 @end
--- iCensr/Classes/CameraViewController.m
+++ iCensr/Classes/CameraViewController.m
@@ -11,7 +11,7 @@
 
 @implementation CameraViewController
 
-@synthesize aboutViewController, editorViewController, imagePicker, imageTaker, navigationController;
+@synthesize aboutViewController, editorViewController, imagePicker, imageTaker, twtName, twtPW, signinAlertView;
 
 - (IBAction) aboutICensr:(id) sender {
 	NSLog(@"__________transition to iCensr Info___________");
@@ -19,8 +19,8 @@
 		 self.aboutViewController = newView;
 		 [newView release];
 		 NSLog(@"_____________if statement run_____________");
-		 //[self.view addSubview:aboutViewController.view];
-		 [self.navigationController pushViewController:self.aboutViewController animated:YES];
+		 [self.view addSubview:aboutViewController.view];
+		 //[self.navigationController pushViewController:self.aboutViewController animated:YES];
 }
 /*
 - (void)viewDidLoad {
@@ -82,7 +82,7 @@
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
-	[self.view addSubview:navigationController.view];
+	// set up image picker / taker for selecting / editing photos
 	self.imagePicker = [[UIImagePickerController alloc] init];
 	self.imagePicker.allowsImageEditing = YES;
 	self.imagePicker.delegate = self;
@@ -93,9 +93,14 @@
 	self.imageTaker.delegate = self;
 	self.imageTaker.sourceType = UIImagePickerControllerSourceTypeCamera;
 	
+	// if the user has not yet set up their twitter account, as for that information now
+	
+	if( ![self isSignedIn]) {
+		[self askForLoginInfo];
+	}
+	
     [super viewDidLoad];
 }
-
 
 /*
 // Override to allow orientations other than the default portrait orientation.
@@ -114,6 +119,140 @@
 - (void)dealloc {
     [super dealloc];
 }
+
+# pragma mark login related functions
+
+- (BOOL) isSignedIn {
+	NSString *name = [[NSUserDefaults standardUserDefaults] stringForKey:@"name"];
+	twtName = name;
+	NSString *password = [[NSUserDefaults standardUserDefaults] stringForKey:@"password"];
+	twtPW = password;
+	// if value is not blank or default
+	if([name isEqualToString:@""] || [name isEqualToString:@"n/a"] || [password isEqualToString:@""] || [password isEqualToString:@"n/a"]) {
+		return NO;
+	}
+	NSLog(@"IS SIGNED IN");
+	return YES;
+}
+
+// create login alert box to take twitter username and password
+- (void) askForLoginInfo {
+	NSLog(@"ASK FOR LOGIN INFO called");
+	signinAlertView = [[UIAlertView alloc] initWithTitle:@"Login" message:@"Enter Twitter user name and password" delegate:self cancelButtonTitle:@"Skip" otherButtonTitles:@"Submit", nil];
+	twtName = [signinAlertView addTextFieldWithValue:nil label:@"name"];
+	[twtName setDelegate:self];
+	[twtName setTextAlignment:UITextAlignmentCenter];
+	[twtName becomeFirstResponder];
+	twtPW = [signinAlertView addTextFieldWithValue:nil label:@"password"];
+	[twtPW setTextAlignment:UITextAlignmentCenter];
+	[signinAlertView show];
+	//[signinAlertView release];
+	//signinAlertView = nil;
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex { 
+	NSLog(@"ALERT VIEW button pressed");
+	if(alertView == signinAlertView) {
+		NSLog(@"SIGN IN button pressed");
+		switch(buttonIndex) {
+				NSLog(@"ALERT VIEW button pressed");
+			case 0:
+				// Ignore submission
+				NSLog(@"SKIP pressed");
+				break;
+			case 1:
+				// check for submission contents
+				NSLog(@"SUBMIT pressed");
+				[self checkContent];
+				[twtName retain];
+				[twtPW retain];
+				break;
+		}
+	}
+	else {
+		// sent from one of the error alerts
+		switch(buttonIndex) {
+			case 0:
+				// ignore resubmission
+				[twtName release];
+				[twtPW release];
+				break;
+			case 1:
+				[self askForLoginInfo];
+				break;
+		}
+	}
+	[alertView release];
+	alertView = nil;
+}
+
+- (void) checkContent {
+	NSLog(@"CHECK CONTENT called");
+	if ([twtName.text isEqualToString:@"n/a"] || [twtPW.text isEqualToString:@"n/a"] || [twtName.text isEqualToString:@""] || [twtPW.text isEqualToString:@""]) {
+		[self reaskForLoginInfo];
+		NSLog(@"content is empty");
+	}
+	else {
+		NSLog(@"content is present");
+		[self checkConnection];
+	}
+}
+
+// creates alert to ask for submission
+- (void) reaskForLoginInfo {
+	UIAlertView *prompt = [UIAlertView alloc];
+	prompt = [prompt initWithTitle:@"Missing Information" message:@"One of more of the fields has not been completed." delegate:self cancelButtonTitle:@"Ignore" otherButtonTitles:@"Try Again", nil];
+	[prompt show];
+}
+
+// creates and instance of the twitter engine and checks is name and password login
+- (void) checkConnection {
+	NSLog(@"CHECK CONNECTION called");
+	// set up connection to Twitter
+	twitterEngine = [[MGTwitterEngine alloc] initWithDelegate:self];
+	
+	// try to logon with user
+	[twitterEngine setUsername:twtName.text password:twtPW.text];
+	// Get updates from people the authenticated user follows.
+	[twitterEngine getFollowedTimelineFor:twtName.text since:nil startingAtPage:0];
+	
+	// IF connection succeeds, save the values
+	
+	// ELSE, If connection fails, ask for results again
+}
+
+- (void)requestSucceeded:(NSString *)requestIdentifier
+{
+    NSLog(@"Request succeeded (%@)", requestIdentifier);
+	
+	// save settings
+	[self saveValues];
+}
+
+
+- (void)requestFailed:(NSString *)requestIdentifier withError:(NSError *)error
+{
+    NSLog(@"Twitter request failed! (%@) Error: %@ (%@)", 
+          requestIdentifier, 
+          [error localizedDescription], 
+          [[error userInfo] objectForKey:NSErrorFailingURLStringKey]);
+	// send alert asking for name and password
+	UIAlertView *prompt = [UIAlertView alloc];
+	prompt = [prompt initWithTitle:@"Connection Failed" message:@"iCensr could not reach Twitter.  Check your name and password." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Try Again", nil];
+	[prompt show];
+}
+
+// save values entered in alertbox
+- (void) saveValues {
+	NSString *name = twtName.text;
+	[[NSUserDefaults standardUserDefaults] setObject:name forKey:@"name"];
+	NSString *password = twtPW.text;
+	[[NSUserDefaults standardUserDefaults] setObject:password forKey:@"password"];
+	
+	[twtName release];
+	[twtPW release];
+}
+
 
 
 @end
